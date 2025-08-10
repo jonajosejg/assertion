4ff099714a49b878643c189178a66f9b183e3725
jonajosejg <rojii0xff@gmail.com>
2025-08-10 14:05:54 -0700

    setup: initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from setuptools import setup
+
+
+setup(name='assertion',
+      version='0.0.1',
+      description='Assert Library for python',
+      author='JJGG',
+      license='MIT',
+      install_requires=[],
+      python_requires='>=3.6',
+)
